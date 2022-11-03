0ae8189569bad5dbffbb743dc5e697edf5979a6a
Dharmik Puri Goswami <dharmikpuri2004@gmail.com>
2022-11-03 15:47:12 +0530

    code fixed by dharmik

--- prime.js
+++ prime.js
@@ -1,9 +1,19 @@
-function checjPrime(n){
-for(let i=0;i<n;i++){
-if(n%i==0){
-count++;
+function checkPrime(num){
+  let factor=0
+   for(let i=0;i<=num;i++){
+     if(num%i==0){
+       factor++
+     }
+   }
+  if(factor==2){
+    return true;
+  }else{
+    return false;
+  }
 }
+let answer=checkPrime(12);
+if(answer == true){
+  console.log("Num is Prime")
+}else{
+  console.log("Num is not prime")
 }
-console.log(count);
-}
-checkprime();
