a5e23736948e3bb240015d3270f97de46d5d637e
krishna668 <narigekumar9@gmail.com>
2022-11-03 16:01:05 +0530

    i also fixed code

--- prime.js
+++ prime.js
@@ -1,9 +1,21 @@
-function checjPrime(n){
-for(let i=0;i<n;i++){
+function checkPrime(n){
+let count = 0;
+for(let i=1;i<=n;i++){
 if(n%i==0){
 count++;
 }
 }
-console.log(count);
+if(count==2){
+return true;
+}else{
+return false;
 }
-checkprime();
+}
+
+let answer=checkPrime(17)
+if(answer==true){
+console.log("Number is Prime")
+}
+else{
+console.log("Number is not a Prime")
+}
